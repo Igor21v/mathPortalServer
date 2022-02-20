bf9a10e3bacc43ad55e56acb32534a2545084855
Igor21v <igor21v@mail.ru>
2022-02-20 15:54:52 +0800

    123

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -17,12 +17,12 @@ class FileController {
                 await fileService.createDir(req, file)
             } else {
                 file.path = `${parentFile.path}\\${file.name}`
-                await fileService.createDir(file)
+                await fileService.createDir(req, file)
                 parentFile.childs.push(file._id)
                 await parentFile.save()
             }
             await file.save()
-            return res.json(req, file)
+            return res.json(file)
         } catch (e) {
             console.log(e)
             return res.status(400).json(e)
