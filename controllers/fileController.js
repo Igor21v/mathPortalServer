a208eec201e5707a6206a026cb00cdc2e84a76ca
Igor21v <igor21v@mail.ru>
2022-02-28 21:52:49 +0800

    add writeFile

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -7,6 +7,29 @@ const Uuid = require('uuid')
 
 
 class FileController {
+    
+    async writeFile(req, res) {
+        try {
+            const {name, type, parent} = req.body
+            const file = new File({name, type, parent, user: req.user.id})
+            const parentFile = await File.findOne({_id: parent})
+            if(!parentFile) {
+                file.path = name
+                await fileService.createDir(req, file)
+            } else {
+                file.path = `${parentFile.path}\\${file.name}`
+                await fileService.createDir(req, file)
+                parentFile.childs.push(file._id)
+                await parentFile.save()
+            }
+            await file.save()
+            return res.json(file)
+        } catch (e) {
+            console.log(e)
+            return res.status(400).json(e)
+        }
+    }
+    
     async createDir(req, res) {
         try {
             const {name, type, parent} = req.body
